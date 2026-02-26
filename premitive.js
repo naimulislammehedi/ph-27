a190024931115bd758d39f8443ffc5bbe96f20fc
Mehedi <mehedi.interface@gmail.com>
2026-02-27 04:09:35 +0600

    27-0 Module overview, primitive and non-primitive data types

---
+++ premitive.js
@@ -0,0 +1,18 @@
+let data = 4; 
+data = 'Tormuj'; 
+data = false; 
+data = {price: 22}
+data = []
+console.log(typeof data); 
+
+// primitive data types 
+let a = 4; 
+let b = "Tormuj"; 
+let c = false; 
+let d = null; 
+let e = undefined; 
+let f = Symbol('id'); 
+
+// non-primitive data types 
+let d = {price: 22}; 
+let e = [1, 2, 3, 4]
