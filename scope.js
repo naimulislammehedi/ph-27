9cf7265c7478f0becc92aa8035114551a28f0036
Mehedi <mehedi.interface@gmail.com>
2026-02-27 12:01:55 +0600

    27-4 Block scope, global scope, simple understanding of hoisting

---
+++ scope.js
@@ -0,0 +1,42 @@
+let pi = 3.14; 
+
+add(2, 5)
+function add (a, b) {
+    const factor = 0.5;
+    const result = (a + b) * factor + pi; 
+    const total = doubleIt(result); 
+
+    function addTwo(num) {
+        num = num + pi; 
+        return num + webkitURL; 
+    }
+    return result
+}
+
+// console.log(factor, pi); 
+add(10, 5)
+
+function doubleIt(num) {
+    return num * 2; 
+}
+
+addTwo(5); 
+
+const multiply = (a, b) => {
+    console.log(result);
+    // TDZ: 
+    const result = a * b; 
+    return result; 
+}
+
+multiply(2, 5); 
+
+if(true) {
+
+}
+
+while(true) {
+
+}
+
+const age = 21; 
